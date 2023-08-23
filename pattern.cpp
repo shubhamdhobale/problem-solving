68bfac18c1d250a57ecdf9977968728cf20337d3
Shubham Dhobakle <97725662+shubhamdhobale@users.noreply.github.com>
2023-08-24 00:03:17 +0530

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -99,7 +99,7 @@ for (int i = 1; i <= n; i++)
 {
     for (int j = 1; j <= i; j++)
     { 
-        cout << count << " " ;
+        cout << count << " " ;       //For reverse => cout << i-j+1 << " " ;
         count++;
     }
     cout << endl;
