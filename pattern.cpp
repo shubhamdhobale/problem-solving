7f44531a45f3137e5cfcaefff770919cc4a9e03d
Shubham Dhobakle <97725662+shubhamdhobale@users.noreply.github.com>
2023-08-24 18:49:41 +0530

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -112,5 +112,198 @@ for (int i = 1; i <= n; i++)
 7 8 9 10
 11 12 13 14 15
 */
+
+/*------------------------------------------------------------------*/
+
+    int n;
+    cin >> n;
+    int row = 1;
+    while (row <= n)
+    {
+        int cols = 1;
+        while (cols <= n)
+        {
+            char ch = 'A' + row - 1;
+            cout << ch << " ";
+            cols++;
+        }
+        cout << endl;
+        row++;
+    }
+    /*
+    ---Output----
+    A A A A A
+    B B B B B
+    C C C C C
+    D D D D D
+    E E E E E
+    */
+    /*------------------------------------------------------------------*/
+    int n;
+    cin >> n;
+    int row = 1;
+    char start = 'A';
+    while (row <= n)
+    {
+        int cols = 1;
+        while (cols <= n)
+        {
+            cout << start << " ";
+            start++;
+
+            cols++;
+        }
+        cout << endl;
+        row++;
+    }
+    /*
+    ---Output----
+A B C D E
+F G H I J
+K L M N O
+P Q R S T
+U V W X Y
+*/
+    /*------------------------------------------------------------------*/
+    int n;
+    cin >> n;
+    int row = 1;
+    while (row <= n)
+    {
+        int cols = 1;
+        while (cols <= n)
+        {
+            char start = 'A' + row + cols - 2;
+            cout << start << " ";
+            start++;
+
+            cols++;
+        }
+        cout << endl;
+        row++;
+    }
+    /*
+    ---Output----
+A B C D E
+B C D E F
+C D E F G
+D E F G H
+E F G H I
+*/
+    /*------------------------------------------------------------------*/
+    int n;
+    cin >> n;
+    int row = 1;
+    while (row <= n)
+    {
+        int cols = 1;
+        while (cols <= row)
+        {
+            char start = 'A' + row - 1;
+            cout << start << " ";
+            start++;
+
+            cols++;
+        }
+        cout << endl;
+        row++;
+    }
+    /*
+    ---Output----
+A
+B B
+C C C
+D D D D
+E E E E E
+*/
+    /*------------------------------------------------------------------*/
+    int n;
+    cin >> n;
+    int row = 1;
+    while (row <= n)
+    {
+        int cols = 1;
+        while (cols <= row)
+        {
+            char start = 'A' + row + cols - 2;
+            cout << start << " ";
+            start++;
+
+            cols++;
+        }
+        cout << endl;
+        row++;
+    }
+    /*
+    ---Output----
+A
+B C
+C D E
+D E F G
+E F G H I
+*/
+    /*------------------------------------------------------------------*/
+    int n;
+    cin >> n;
+    int row = 1;
+    while (row <= n)
+    {
+        int cols = 1;
+        char start = 'A' + n - row;
+
+        while (cols <= row)
+        {
+            cout << start << " ";
+            start++;
+            cols++;
+        }
+        cout << endl;
+        row++;
+    }
+    /*
+         ---Output----
+     E
+     D E
+     C D E
+     B C D E
+     A B C D E
+     */
+
+    /*------------------------------------------------------------------*/
+    int n;
+    cin >> n;
+    int row = 1;
+    while (row <= n)
+    {
+        int space = n - row;
+        while (space)
+        {
+            cout << " ";
+            space = space - 1;
+        }
+        int j = 1;
+        while (j <= row)
+        {
+            cout << j;
+            j++;
+        }
+        int start = row - 1;
+        while (start)
+        {
+            cout << start;
+            start--;
+        }
+        cout << endl;
+        row ++;
+    }
+    /*
+    ---Output----
+   1
+  121
+ 12321
+1234321
+123454321
+*/
+
     return 0;
 }
